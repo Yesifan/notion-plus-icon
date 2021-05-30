c14b09acfe7bfa21e19eabbd586543074e955a0c
YeSifan <yesifan66@gmail.com>
2021-05-30 18:16:08 +0800

    chore:storage 变化监听

--- src/content/react/hooks.ts
+++ src/content/react/hooks.ts
@@ -7,15 +7,22 @@ const { storage } = chrome;
 export const useGetStorage = () => {
   const isDestory = useRef(false);
   const [ urls, setUrls ] = useState<string[]>([]);
-  useEffect(()=>{
+  const setUrl = () => {
     storage.sync.get(ICON_STORAGE_KEY, items => {
       if(isDestory.current) return;
       const urls = items[ICON_STORAGE_KEY];
       setUrls(urls);
     })
+  }
+  useEffect(()=>{
+    setUrl();
+    storage.onChanged.addListener(setUrl)
     return () => {
       isDestory.current = true;
+      storage.onChanged.removeListener(setUrl);
     }
-  });
+  }, []);
+
+
   return urls;
 }
