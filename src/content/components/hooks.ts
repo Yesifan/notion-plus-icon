8f6fb9d9fbf3be65a203c3be37756fb8f2c23c19
YeSifan <yesifan66@gmail.com>
2021-05-30 12:31:30 +0800

    fixed:hooks 内存泄露

--- src/content/components/hooks.ts
+++ src/content/components/hooks.ts
@@ -1,15 +1,21 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 import { ICON_STORAGE_KEY } from '@/api/notion/icon';
 
+const { storage } = chrome;
+
 export const useGetStorage = () => {
-  const { storage } = chrome;
+  const isDestory = useRef(false);
   const [ urls, setUrls ] = useState<string[]>([]);
   useEffect(()=>{
     storage.sync.get(ICON_STORAGE_KEY, items => {
+      if(isDestory.current) return;
       const urls = items[ICON_STORAGE_KEY];
       setUrls(urls);
     })
+    return () => {
+      isDestory.current = true;
+    }
   });
   return urls;
 }
