34cbe1fa43ef99d8154a504fd3fd80c425b87644
YeSifan <yesifan66@gmail.com>
2021-05-30 11:34:00 +0800

    chore:row rank

--- src/content/components/panel/index.tsx
+++ src/content/components/panel/index.tsx
@@ -9,7 +9,9 @@ import Upload from '../upload';
 import setIcon4pageId from '@/api/notion/icon'
 
 import * as style from './css';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
+
+const ROW_SIZE = 12;
 
 export interface PanelProps {
   pageId: string
@@ -19,7 +21,16 @@ export interface PanelProps {
 const App:React.FC<PanelProps> = ({pageId, container}) => {
   const urls = useGetStorage();
   const setIcon = useCallback((icon:string)=>setIcon4pageId(pageId, icon), [pageId]);
-
+  const wrap = useMemo(()=>{
+    return urls.reduce<string[][]>((acc, url)=>{
+      const current = acc[acc.length-1];
+      if(current.length >= ROW_SIZE){
+        acc.push([url])
+      }else{
+        current.push(url)
+      }
+      return acc;
+    },[[]])}, [urls]);
   return createPortal(
     <div style={style.columnFlex}>
       <div style={style.toolRow}>
@@ -29,14 +40,16 @@ const App:React.FC<PanelProps> = ({pageId, container}) => {
       <div style={{flexGrow: 1}}>
         <div style={style.padding}>
           <div style={style.iconContainer}>
-            {urls.map(url => (
+            {wrap.map((row, index) => (
+            <div style={{display:'flex'}} key={index}>
+              {row.map(url => (
               <Hover key={url} className="notion-focusable" role="button" tabIndex={-1} style={style.icon} onClick={()=>setIcon(url)}>
                 <img alt="img-url" aria-label="img-url"
                   className="notion-emoji"
                   style={style.img}
                   src={url} />
-              </Hover>
-            ))}
+              </Hover>))}
+            </div>))}
           </div>
         </div>
         <div></div>
