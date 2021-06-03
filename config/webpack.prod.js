027e23c903f3ccb67ec9b81343c02e1a71232ced
yesifan <yesifan66@gmail.com>
2021-06-03 17:55:22 +0800

    chore:update webpack config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,34 +1,15 @@
-const webpack = require("webpack");
 const CopyPlugin = require("copy-webpack-plugin");
 
 const paths = require("./paths");
 
 const config = {
-  mode: 'development',
+  mode: 'production',
   entry: paths.entry,
-  devtool: 'cheap-module-source-map',
   output: {
     path: paths.build,
     filename: "[name].js",
     chunkFilename:"static/js/[name].chunk.js",
-    publicPath: 'https://localhost:3333/',
     clean: true
-  },
-  devServer:{
-    hot:true,
-    hotOnly: true,
-    port: 3333,
-    https: true,
-    writeToDisk: true,
-    allowedHosts: ['.notion.so'],
-    setup(app){
-      app.all('*', function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-        next();
-      });
-    }
   },
   resolve:{
     alias: {
@@ -54,7 +35,6 @@ const config = {
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new CopyPlugin({
       patterns: [{ from: "public", to: "." }]
     }),
